24a353bf24f3c487b7f7cdeed04ec1ffe4a4345d
Leonardo Jacomussi <leonardojacomussi@gmail.com>
2024-02-25 10:45:41 -0300

    build: test rootPath

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -6,16 +6,20 @@ import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 
+// const rootPath = process.env.NODE_ENV === 'development'
+//   ? join(__dirname, '../../../apps/frontend/dist')
+//   : join(__dirname, '../../apps/frontend/dist');
+
 const rootPath = process.env.NODE_ENV === 'development'
   ? join(__dirname, '../../../apps/frontend/dist')
-  : join(__dirname, '../../apps/frontend/dist');
+  : join(__dirname, '../../frontend/dist');
 
 console.log('rootPath:', rootPath);
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '../../apps/frontend/dist'),
+      rootPath: join(__dirname, '../../../apps/frontend/dist'),
       exclude: ['api/*'],
     }),
   ],
